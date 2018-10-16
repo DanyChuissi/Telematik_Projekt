058630d09f753333335a824dd6e7c48e165b9e01
Dany Chuissi <dachu001@fh-dortmund.de>
2018-10-16 18:18:18 +0200

    Fachkonzept und GUI Paket erstellt

---
+++ src/Fachkonzept/Patient.java
@@ -0,0 +1,15 @@
+Public Class Patient{
+    private String name ;
+    private String vorname;
+
+    public Patient(String name, String vorname){
+        this.name = name;
+        this.vorname = Vorname;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
+        public String getNeme(){
+            return this name;
+        }
+    }
