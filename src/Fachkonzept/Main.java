fbb08c18517c6bccc9ba4ea4edf5bfaa738c5a12
Dany Chuissi <dachu001@fh-dortmund.de>
2018-10-18 17:01:49 +0200

    iml datei für andere Mitglied

---
+++ src/Fachkonzept/Main.java
@@ -0,0 +1,9 @@
+package src.Fachkonzept;
+
+public class Main {
+    public static void main(String[] args) {
+        Patient p = new Patient("Dany", "Chuissi");
+        System.out.println(p.getName());
+
+    }
+}
--- src/Fachkonzept/Patient.java
+++ src/Fachkonzept/Patient.java
@@ -1,15 +1,23 @@
-Public Class Patient{
+
+
+
+package src.Fachkonzept;
+
+public class Patient{
     private String name ;
     private String vorname;
 
     public Patient(String name, String vorname){
         this.name = name;
-        this.vorname = Vorname;
-        }
-        public void setName(String name){
-            this.name = name;
-        }
-        public String getNeme(){
-            return this name;
-        }
+        this.vorname = vorname;
     }
+    public void setName(String name){
+            this.name = name;
+    }
+    public String getName(){
+            return this.name;
+    }
+
+
+
+}
