72aac90be15108768293f47fa2a5d9240bcd2991
maz-sb <homaz001>
2018-10-21 23:59:15 +0200

    sehe ob ich rictig gemacht

--- src/Fachlogik/Medikament.java
+++ src/Fachlogik/Medikament.java
@@ -1,190 +1,193 @@
 package src.Fachlogik;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Medikament {
 
-    private long identifier;
-    private long basedOn;
-    private long partOf;
-    private long context;
-    private long status;
-    private long category;
-    private int medication [];
-    private int effective[];
-    private long dateAsserted;
-    private long informationSource;
-    private long subject;
-    private long derivedForm;
-    private long taken;
-    private long reasonNotTaken;
-    private long reasonCode;
-    private long reasonReference;
-    private long note;
-    private long dosage;
+   ;
+    private List<String> basedOn;
+    private List<String> partOf;
+    private boolean context;
+    private boolean status;
+    private boolean category;
+    private boolean medication;
+    private boolean effective;
+    private int dateAsserted;
+    private String informationSource;
+    private boolean subject;
+    private List<String> derivedForm;
+    private boolean taken;
+    private List<String> reasonNotTaken;
+    private List<String> reasonCode;
+    private List<String> reasonReference;
+    private List<String> note;
+    private List<String> dosage;
+    private List<Long> identifier;
 
-    public Medikament( long identifier, long basedOn, long partOf,long context, long status, long category, long dateAsserted, long informationSource, long subject, long derivedForm, long taken, long reasonNotTaken, long reasonCode,long reasonReference, long note, long dosage){
-        this.identifier = identifier;
-        this.basedOn= basedOn;
-        this.partOf= partOf;
-        this.context=context;
-        this.status=status;
-        this.category=category;
-        this.dateAsserted=dateAsserted;
-        this.informationSource=informationSource;
+
+    public Medikament(boolean status, boolean medication, boolean subject,boolean taken){
+        identifier = new ArrayList<>();
+        basedOn = new ArrayList<>();
+        partOf = new ArrayList<>();
+        derivedForm = new ArrayList<>();
+        reasonReference= new ArrayList<>();
+        reasonNotTaken = new ArrayList<>();
+        reasonCode = new ArrayList<>();
+        note = new ArrayList<>();
+        dosage = new ArrayList<>();
+        this.medication=medication;
+        this.status= status;
         this.subject=subject;
-        this.derivedForm=derivedForm;
         this.taken=taken;
-        this.reasonNotTaken=reasonNotTaken;
-        this.reasonCode=reasonCode;
-        this.reasonReference=reasonReference;
-        this.note=note;
-        this.dosage=dosage;
+
 
 
     }
-
-    public long getIdentifier() {
-        return identifier;
+    public void addIdentifier( long newIdentifier){
+        identifier.add(newIdentifier);
+    }
+    public void addBasedOn (String newBesedOn ){
+        basedOn.add(newBesedOn);
+    }
+    public void addPartOf ( String newPartOf){
+        partOf.add(newPartOf);
+    }
+    public void addDrivedForm( String newDrivedFrom){
+        derivedForm.add(newDrivedFrom);
+    }
+    public void addReasonReference( String newReasonReference){
+        reasonReference.add(newReasonReference);
+    }
+    public void addReasonNotTaken( String newReasonNotTaken){
+        reasonReference.add(newReasonNotTaken);
+    }
+    public void addReasonCode( String newReasoncode){
+        reasonCode.add(newReasoncode);
+    }
+    public void addNote( String newNote){
+        note.add(newNote);
+    }
+    public void addDosage( String newDosage){
+        dosage.add(newDosage);
     }
 
-
-    public long getBasedOn() {
-        return basedOn;
+    public void setTaken(boolean taken) {
+        this.taken = taken;
     }
 
-
-    public long getCategory() {
-        return category;
-    }
-
-    public long getContext() {
-        return context;
-    }
-
-    public long getDateAsserted() {
-        return dateAsserted;
-    }
-
-    public long getDerivedForm() {
-        return derivedForm;
-    }
-
-    public long getDosage() {
-        return dosage;
-    }
-
-    public long getInformationSource() {
-        return informationSource;
-    }
-
-    public long getNote() {
-        return note;
-    }
-
-    public long getPartOf() {
-        return partOf;
-    }
-
-    public long getReasonCode() {
-        return reasonCode;
-    }
-
-    public long getReasonNotTaken() {
-        return reasonNotTaken;
-    }
-
-    public long getReasonReference() {
-        return reasonReference;
-    }
-
-    public long getStatus() {
-        return status;
-    }
-
-    public long getSubject() {
-        return subject;
-    }
-
-    public long getTaken() {
-        return taken;
-    }
-
-    public int[] getEffective() {
-        return effective;
-    }
-
-    public int[] getMedication() {
-        return medication;
-    }
-
-    public void setBasedOn(long basedOn) {
-        this.basedOn = basedOn;
-    }
-
-    public void setIdentifier(long identifier) {
-        this.identifier = identifier;
-    }
-
-    public void setCategory(long category) {
-        this.category = category;
-    }
-
-    public void setContext(long context) {
-        this.context = context;
-    }
-
-    public void setDateAsserted(long dateAsserted) {
-        this.dateAsserted = dateAsserted;
-    }
-
-    public void setDerivedForm(long derivedForm) {
-        this.derivedForm = derivedForm;
-    }
-
-    public void setDosage(long dosage) {
-        this.dosage = dosage;
-    }
-
-    public void setEffective(int[] effective) {
-        this.effective = effective;
-    }
-
-    public void setInformationSource(long informationSource) {
-        this.informationSource = informationSource;
-    }
-
-    public void setMedication(int[] medication) {
-        this.medication = medication;
-    }
-
-    public void setNote(long note) {
-        this.note = note;
-    }
-
-    public void setPartOf(long partOf) {
-        this.partOf = partOf;
-    }
-
-    public void setReasonCode(long reasonCode) {
-        this.reasonCode = reasonCode;
-    }
-
-    public void setReasonNotTaken(long reasonNotTaken) {
-        this.reasonNotTaken = reasonNotTaken;
-    }
-
-    public void setReasonReference(long reasonReference) {
-        this.reasonReference = reasonReference;
-    }
-
-    public void setStatus(long status) {
-        this.status = status;
-    }
-
-    public void setSubject(long subject) {
+    public void setSubject(boolean subject) {
         this.subject = subject;
     }
 
-    public void setTaken(long taken) {
-        this.taken = taken;
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public void setMedication(boolean medication) {
+        this.medication = medication;
+    }
+
+    public void setContext(boolean context) {
+        this.context = context;
+    }
+
+    public void setEffective(boolean effective) {
+        this.effective = effective;
+    }
+
+    public void setDateAsserted(int dateAsserted) {
+        this.dateAsserted = dateAsserted;
+    }
+
+    public void setCategory(boolean category) {
+        this.category = category;
+    }
+
+    public void setInformationSource(String informationSource) {
+        this.informationSource = informationSource;
+    }
+
+    public boolean isCategory() {
+        return category;
+    }
+
+    public boolean isTaken() {
+        return taken;
+    }
+
+    public boolean isContext() {
+        return context;
+    }
+
+    public boolean isSubject() {
+        return subject;
+    }
+
+    public boolean isEffective() {
+        return effective;
+    }
+
+    public boolean isMedication() {
+        return medication;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public int getDateAsserted() {
+        return dateAsserted;
+    }
+
+    public String getInformationSource() {
+        return informationSource;
+    }
+
+    public List<Long> getIdentifier() {
+        return identifier;
+    }
+
+    public List<String> getDerivedForm() {
+        return derivedForm;
+    }
+
+    public void setReasonNotTaken(List<String> reasonNotTaken) {
+        this.reasonNotTaken = reasonNotTaken;
+    }
+
+    public void setPartOf(List<String> partOf) {
+        this.partOf = partOf;
+    }
+
+    public void setReasonReference(List<String> reasonReference) {
+        this.reasonReference = reasonReference;
+    }
+
+    public void setBasedOn(List<String> basedOn) {
+        this.basedOn = basedOn;
+    }
+
+    public void setReasonCode(List<String> reasonCode) {
+        this.reasonCode = reasonCode;
+    }
+
+    public void setDerivedForm(List<String> derivedForm) {
+        this.derivedForm = derivedForm;
+    }
+
+    public void setNote(List<String> note) {
+        this.note = note;
+    }
+
+    public void setDosage(List<String> dosage) {
+        this.dosage = dosage;
+    }
+
+    public void setIdentifier(List<Long> identifier) {
+        this.identifier = identifier;
     }
 }
+
+
+
